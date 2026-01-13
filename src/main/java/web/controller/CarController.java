9efbca8b9e3e9b344acfeb4ca9e29da00662f287
Polina <polinamur@yandex.ru>
2026-01-13 13:28:10 +0300

    CarController & package service changed

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,19 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
+import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
 
-    private final CarService carService;
+    private final CarServiceImpl carService;
 
-    public CarController(CarService carService) {
+    public CarController(CarServiceImpl carService) {
         this.carService = carService;
     }
 
     @GetMapping("/cars")
-    public String GetCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+    public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
         model.addAttribute("carList", carService.getCars(count));
         return "cars";
     }
